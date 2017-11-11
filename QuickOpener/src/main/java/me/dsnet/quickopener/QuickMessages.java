f17b1dd686c466060d4968779bd3554c8b9615e4
Javatlacati <>
2017-11-11 07:10:15 -0600

    Attenfing issue #80. Adding license headers to all sources.

--- QuickOpener/src/main/java/me/dsnet/quickopener/QuickMessages.java
+++ QuickOpener/src/main/java/me/dsnet/quickopener/QuickMessages.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017 Diego Zambelli Sessona (diego.sessona@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package me.dsnet.quickopener;
 
@@ -18,6 +30,7 @@ public enum QuickMessages {
     FOLDER_ADDED ("Folder added as favorite."),
     SEPARATOR_NULL ("Separator cannot be empty."),
     CUSTOM_SHELL ("Custom shell cannot be empty."),
+    CUSTOM_FILEMANAGER ("Custom file manager cannot be empty."),
     NO_COMMAND ("No command given."),
     NO_DEFAULT_PARAMETERS ("Some placeholders could not be replaced."),
     DEFAULT_COMMAND_PARAMETERS ("A default command cannot contain parameters."),
