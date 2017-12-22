e016cea299d59fda3a754d3e7b90ad72ce8f5d78
markiewb <benno.markiewicz@googlemail.com>
2017-12-22 15:14:05 +0100

    PR-Cleanup: Do not only include options for a custom file manager but use it too!
    CP from TerminalAction

--- QuickOpener/src/main/java/me/dsnet/quickopener/actions/FileSystem.java
+++ QuickOpener/src/main/java/me/dsnet/quickopener/actions/FileSystem.java
@@ -3,11 +3,17 @@ package me.dsnet.quickopener.actions;
 import com.sessonad.oscommands.commands.Commands;
 import me.dsnet.quickopener.QuickMessages;
 import java.io.File;
+import me.dsnet.quickopener.PathFinder;
+import me.dsnet.quickopener.prefs.PrefsUtil;
+import me.dsnet.quickopener.prefs.QuickOpenerProperty;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
 import org.openide.util.NbBundle.Messages;
 
@@ -42,16 +48,33 @@ public final class FileSystem extends AbstractFileContextAwareAction {
     @Override
     protected void performAction(Node[] activatedNodes) {
         File file = getFile();
-
-        if (file == null) {
-            NotifyDescriptor d = new NotifyDescriptor.Message(QuickMessages.NO_FILE_IN_SELECTION, NotifyDescriptor.WARNING_MESSAGE);
-            DialogDisplayer.getDefault().notify(d);
-            return;
-        }
+        
         try {
-            Commands.getPlatform().browseInFileSystemToFileOrDir(file);
-        } catch (Exception ex) {
-        }//ex.printStackTrace();}        
-    }
+            if (file == null) {
+                NotifyDescriptor d = new NotifyDescriptor.Message(QuickMessages.NO_FILE_IN_SELECTION, NotifyDescriptor.WARNING_MESSAGE);
+                DialogDisplayer.getDefault().notify(d);
+                return;
+            }
+            FileObject fo = FileUtil.toFileObject(FileUtil.normalizeFile(file));
+            DataObject dataObj = DataObject.find(fo);
+            String path = PathFinder.getActivePath(dataObj, true);
+            if (path == null) {
+                NotifyDescriptor d = new NotifyDescriptor.Message(QuickMessages.NO_FILE_IN_SELECTION, NotifyDescriptor.WARNING_MESSAGE);
+                DialogDisplayer.getDefault().notify(d);
+                return;
+            }
 
+            //open in os shell or in the custom one if defined
+            QuickOpenerProperty customShell = PrefsUtil.load(null, "customFileManager", null);
+            if (customShell.getValue() == null) {
+                Commands.getPlatform().browseInFileSystemToFileOrDir(file);
+            } else {
+                Runtime.getRuntime().exec(customShell.getValue(), null, new File(path));
+            }
+
+        } catch (Exception ex) {
+        }//ex.printStackTrace();}
+     
+    }
+    
 }
